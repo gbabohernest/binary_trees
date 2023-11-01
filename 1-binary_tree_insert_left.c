a1fcd128bc1beaa16ffa2c0f4bc4f96a44a31df2
Ernest <gbaboh.ernest@gmail.com>
2023-11-01 21:06:16 +0000

    fix betty style bad line

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,7 +5,7 @@
 *			left-child of another node
 * @parent: A pointer to the node to insert the left-child in
 * @value: Is the value to store in the new node
-
+*
 * Return: A pointer to the created node |
 *	  NULL on failure | if parent is NULL
 */
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *new_node, *tmp;
+	binary_tree_t *new_node, *old_left;
 
 	new_node = binary_tree_node(parent, value);
 	/*new_node = malloc(sizeof(binary_tree_t));*/
@@ -32,10 +32,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	else
 	{
-		tmp = parent->left;
+		old_left = parent->left;
 		parent->left = new_node;
-		new_node->left = tmp;
-		tmp->parent = new_node;
+		new_node->left = old_left;
+		old_left->parent = new_node;
 	}
 	return (new_node);
 }
