c91a61c6e65c506a361142074d2b13d67a7277e3
Ernest <gbaboh.ernest@gmail.com>
2023-11-02 18:16:27 +0000

    add function to check for num of children nodes and leaf nodes

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
-/*int is_tree_full_recursion(const binary_tree_t *tree);*/
+int is_leaf_node(const binary_tree_t *tree);
+size_t has_children(const binary_tree_t *tree);
+
 /**
 * binary_tree_is_full - Checks if a binary tree is full
 * @tree: A pointer to the root node of the tree to check
@@ -8,20 +10,67 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int  is_left_full, is_right_full;
+	/*int  is_left_full, is_right_full,*/
+	int is_leafNode;
 
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right) ||
-		(tree->left && tree->right == NULL))
+	/*
+	 * if ((tree->left == NULL && tree->right) ||
+	 *	(tree->left && tree->right == NULL) ||
+	 *	binary_tree_is_full(tree->left) == 0 ||
+	 *	binary_tree_is_full(tree->right) == 0)
+	*/
+		/*return (0);*/
+
+	/*if (tree->left && tree->right)*/
+	/*{*/
+		/*is_left_full = binary_tree_is_full(tree->left);*/
+		/*is_right_full = binary_tree_is_full(tree->right);*/
+	/*}*/
+
+	/*return (is_left_full && is_right_full);*/
+	is_leafNode = is_leaf_node(tree);
+	if (is_leafNode)
+		return (1);
+
+	if (has_children(tree))
+	{
+		return (binary_tree_is_full(tree->left) * binary_tree_is_full(tree->right));
+	}
+	return (0);
+}
+
+
+/**
+* is_leaf_node - Checks if a node is a leaf node
+*		(no children) at left and right
+* @tree: A pointer to the root node of the tree to check
+* Return: 1 if node is a leaf, 0 if not
+*/
+
+int is_leaf_node(const binary_tree_t *tree)
+{
+	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right)
-	{
-		is_left_full = binary_tree_is_full(tree->left);
-		is_right_full = binary_tree_is_full(tree->right);
-	}
+	if (tree->left || tree->right)
+		return (0);
 
-	return (is_left_full && is_right_full);
+	return (1);
+}
+
+/**
+* has_children - Checks if a node has two children
+* @tree: A pointer to the root node of the tree to check
+* Return:  1 if there are 2 children, 0 otherwise
+*/
+
+size_t has_children(const binary_tree_t *tree)
+{
+	if (tree->left  && tree->right)
+		return (1);
+
+	return (0);
 }
